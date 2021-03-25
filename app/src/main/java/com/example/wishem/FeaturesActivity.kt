11bee264f7dbf6c92c827c0d561c388e3ebd073f
Kshitij Kumar <kshitij2212@gmail.com>
2021-03-25 13:56:56 +0530

    line romoved

--- app/src/main/java/com/example/wishem/FeaturesActivity.kt
+++ app/src/main/java/com/example/wishem/FeaturesActivity.kt
@@ -18,6 +18,5 @@ class FeaturesActivity : AppCompatActivity() {
     private fun setupViews() {
         //navigation
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
     }
 }
